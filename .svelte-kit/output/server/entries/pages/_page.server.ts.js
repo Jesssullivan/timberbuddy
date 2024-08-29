314bf2329924517d79f774498ee71f3774efe46e
jesssullivan <jess@sulliwood.org>
2024-08-29 13:32:19 -0400

    rm ssr enforce

--- .svelte-kit/output/server/entries/pages/_page.server.ts.js
+++
@@ -1,3 +0,0 @@
-import { d as data_path, a as write_defaults } from "../../chunks/server_file_utils.js";
-import fs from "fs";
-(() => fs.existsSync(data_path) ? console.log("schema found at " + data_path) : write_defaults())();
