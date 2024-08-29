c13897a3c17a6bb4c61c52e8e215446bfdd97969
jesssullivan <jess@sulliwood.org>
2024-08-29 13:32:19 -0400

    rm ssr enforce

--- .svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.25_@sveltejs+vite-plugin-svelte@3.1.2_svelte@4.2.19_vite@5.4.2_@types+node@_6xy3o7bdx7wwmq434mrlb3eb4e/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
--- .svelte-kit/output/server/entries/pages/core/_page.server.ts.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-async function load() {
-  return {
-    read_defaults: await JSON.parse(fs.readFileSync("store.json", "utf-8"))
-  };
-}
-export {
-  load
-};
